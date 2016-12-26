4746e4a2f30147b2cca61938ee36c32e722cec56
Andrew Demchenko <blackdeman@gmail.com>
2016-12-26 23:13:22 +0300

    remove java rs with application server

--- java-rs/src/main/java/by/bsu/dreamteam/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package by.bsu.dreamteam;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("")
-public class JAXRSConfiguration extends Application {
-}
